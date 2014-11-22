b35e60b03de49589e155194381f4b85134a59dc7
Evisceration <eviscerationls@gmail.com>
2014-11-22 08:48:10 +0000

    allow to configure the long press back kill delay (2/2)
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/ButtonSettings.java
+++ src/com/pixeldust/settings/fragments/ButtonSettings.java
@@ -6,6 +6,7 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
@@ -19,32 +20,50 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.pixeldust.settings.preferences.CustomSeekBarPreference;
+
 public class ButtonSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
     private SwitchPreference mKillAppLongPressBack;
 
+    private static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
+    private CustomSeekBarPreference mLongpressKillDelay;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.pixeldust_settings_button);
+        ContentResolver resolver = getActivity().getContentResolver();
 
         // kill-app long press back
         mKillAppLongPressBack = (SwitchPreference) findPreference(KILL_APP_LONGPRESS_BACK);
         mKillAppLongPressBack.setOnPreferenceChangeListener(this);
-        int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
+        int killAppLongPressBack = Settings.Secure.getInt(resolver,
                 KILL_APP_LONGPRESS_BACK, 0);
         mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
+
+        // kill-app long press back delay
+        mLongpressKillDelay = (CustomSeekBarPreference) findPreference(LONG_PRESS_KILL_DELAY);
+        int killconf = Settings.System.getInt(resolver,
+                Settings.System.LONG_PRESS_KILL_DELAY, 1000);
+        mLongpressKillDelay.setValue(killconf);
+        mLongpressKillDelay.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mKillAppLongPressBack) {
             boolean value = (Boolean) newValue;
-            Settings.Secure.putInt(getContentResolver(),
+            Settings.Secure.putInt(resolver,
 		KILL_APP_LONGPRESS_BACK, value ? 1 : 0);
+            return true;
+        } else if (preference == mLongpressKillDelay) {
+            int killconf = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.LONG_PRESS_KILL_DELAY, killconf);
             return true;
         }
         return false;
