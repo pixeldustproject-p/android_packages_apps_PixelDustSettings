b99972f16fa13e3727c311ffe3e222b73845ec79
yank555-lu <yank555.lu@gmail.com>
2018-02-13 09:50:28 -0800

    StatusbarClock: date right/left position [2/2]
    Change-Id: I7207ac70a7310cfe36672ce6cbc699c69006d043
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/StatusBarSettings.java
+++ src/com/pixeldust/settings/fragments/StatusBarSettings.java
@@ -81,6 +81,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     public static final int CLOCK_DATE_STYLE_LOWERCASE = 1;
     public static final int CLOCK_DATE_STYLE_UPPERCASE = 2;
     private static final int CUSTOM_CLOCK_DATE_FORMAT_INDEX = 18;
+    private static final String STATUS_BAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
 
     private SystemSettingSwitchPreference mStatusBarClockShow;
     private SystemSettingSwitchPreference mStatusBarSecondsShow;
@@ -89,6 +90,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private ListPreference mClockDateDisplay;
     private ListPreference mClockDateStyle;
     private ListPreference mClockDateFormat;
+    private ListPreference mClockDatePosition;
 
     private ListPreference mBatteryIconStyle;
     private ListPreference mBatteryPercentage;
@@ -122,6 +124,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
         mClockDateDisplay = (ListPreference) findPreference(STATUS_BAR_CLOCK_DATE_DISPLAY);
         mClockDateStyle = (ListPreference) findPreference(STATUS_BAR_CLOCK_DATE_STYLE);
+        mClockDatePosition = (ListPreference) findPreference(STATUS_BAR_CLOCK_DATE_POSITION);
 
         mStatusBarClockShow.setChecked((Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_CLOCK, 1) == 1));
@@ -172,6 +175,18 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             mClockDateFormat.setValue(clkvalue);
         }
         parseClockDateFormats();
+
+        mClockDatePosition.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.STATUSBAR_CLOCK_DATE_POSITION,
+                0)));
+        mClockDatePosition.setSummary(mClockDatePosition.getEntry());
+        mClockDatePosition.setOnPreferenceChangeListener(this);
+
+        int clockDatePosition = Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_CLOCK_DATE_POSITION, 0);
+        mClockDatePosition.setValue(String.valueOf(clockDatePosition));
+        mClockDatePosition.setSummary(mClockDatePosition.getEntry());
+        mClockDatePosition.setOnPreferenceChangeListener(this);
 
 	// Battery styles
         int batteryStyle = Settings.Secure.getInt(resolver,
@@ -248,9 +263,11 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             if (clockDateDisplay == 0) {
                 mClockDateStyle.setEnabled(false);
                 mClockDateFormat.setEnabled(false);
+                mClockDatePosition.setEnabled(false);
             } else {
                 mClockDateStyle.setEnabled(true);
                 mClockDateFormat.setEnabled(true);
+                mClockDatePosition.setEnabled(true);
             }
             return true;
         } else if (preference == mClockDateStyle) {
@@ -320,6 +337,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                     .findIndexOfValue((String) objValue);
             mBatteryPercentage
                     .setSummary(mBatteryPercentage.getEntries()[valueIndex]);
+            return true;
+        } else if (preference == mClockDatePosition) {
+            int val = Integer.parseInt((String) objValue);
+            int index = mClockDatePosition.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_DATE_POSITION, val);
+            mClockDatePosition.setSummary(mClockDatePosition.getEntries()[index]);
+            parseClockDateFormats();
             return true;
         }
         return false;
