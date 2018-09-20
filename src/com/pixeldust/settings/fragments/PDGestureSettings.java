fb16e0c0f23f1c26c3ba986660070dab93e60665
spezi77 <spezi7713@gmx.net>
2018-09-20 13:53:05 +0200

    Rename GestureSettings to avoid conflicts with class com.android.settings.gestures.GestureSettings
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/PDGestureSettings.java
+++ src/com/pixeldust/settings/fragments/PDGestureSettings.java
@@ -7,7 +7,7 @@ import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class GestureSettings extends SettingsPreferenceFragment {
+public class PDGestureSettings extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle icicle) {
