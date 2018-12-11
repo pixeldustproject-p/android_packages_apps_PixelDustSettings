a4414aef69d06b86912a5d4380794fc2d7bd7c55
spezi77 <spezi7713@gmx.net>
2018-12-11 21:37:14 +0100

    Stock Navbar: Add longpress back to kill app with configurable delay.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/StockNavBarSettings.java
+++ src/com/pixeldust/settings/fragments/StockNavBarSettings.java
@@ -16,22 +16,72 @@
 
 package com.pixeldust.settings.fragments;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 
+import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class StockNavBarSettings extends SettingsPreferenceFragment {
+import com.pixeldust.settings.preferences.CustomSeekBarPreference;
+
+public class StockNavBarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+    private static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
+    private CustomSeekBarPreference mLongpressKillDelay;
+    private SwitchPreference mKillAppLongPressBack;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.stock_settings);
+
+        // kill-app long press back
+        mKillAppLongPressBack = (SwitchPreference) findPreference(KILL_APP_LONGPRESS_BACK);
+        mKillAppLongPressBack.setOnPreferenceChangeListener(this);
+        int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
+                KILL_APP_LONGPRESS_BACK, 0);
+        mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
+
+        // kill-app long press back delay
+        mLongpressKillDelay = (CustomSeekBarPreference) findPreference(LONG_PRESS_KILL_DELAY);
+        int killconf = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.LONG_PRESS_KILL_DELAY, 1000);
+        mLongpressKillDelay.setValue(killconf);
+        mLongpressKillDelay.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mKillAppLongPressBack) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+		KILL_APP_LONGPRESS_BACK, value ? 1 : 0);
+            return true;
+        } else if (preference == mLongpressKillDelay) {
+            int killconf = (Integer) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.LONG_PRESS_KILL_DELAY, killconf);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.PIXELDUST;
     }
+
 }
