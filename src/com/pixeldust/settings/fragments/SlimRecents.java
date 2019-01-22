21d0541570a402c988e48624354f0870d030c185
Giuseppe Maggio <jertlokk@gmail.com>
2019-01-22 18:57:11 +0100

    RecentsIconPack: Extend and use IconPackPreference
    The generic class IconPackPreference allows to have an icon pack dialog picker
    with installed icon packs and have the preference stored - with this commit we
    extend this preference calling the new class RecentsIconPackPreference.
    
    The new class basically overrides persistString and getPersistedString,
    storing the information inside Settings.System.RECENTS_ICON_PACK.
    
    Also removed unused imports from Recents since we are on it.
    
    Change-Id: I1a16df2bd44573dad481fd7962dbde1b7161ea3b
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/SlimRecents.java
+++ src/com/pixeldust/settings/fragments/SlimRecents.java
@@ -17,14 +17,7 @@
 
 package com.pixeldust.settings.fragments;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.content.res.Resources;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
@@ -32,22 +25,13 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.pixeldust.settings.preferences.SystemSettingSwitchPreference;
+import com.pixeldust.support.preferences.IconPackPreference;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -56,27 +40,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SlimRecents extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener, DialogInterface.OnDismissListener {
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String RECENT_PANEL_LEFTY_MODE = "recent_panel_lefty_mode";
 
     private static final String RECENT_ICON_PACK = "slim_icon_pack";
 
     private SwitchPreference mRecentPanelLeftyMode;
-    private Preference mIconPack;
-
-    // Icon pack
-    private final static String[] sSupportedActions = new String[] {
-        "org.adw.launcher.THEMES",
-        "com.gau.go.launcherex.theme"
-    };
-    private static final String[] sSupportedCategories = new String[] {
-        "com.fede.launcher.THEME_ICONPACK",
-        "com.anddoes.launcher.THEME",
-        "com.teslacoilsw.launcher.THEME"
-    };
-    private AlertDialog mDialog;
-    private ListView mListView;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -86,7 +56,6 @@ public class SlimRecents extends SettingsPreferenceFragment
 
         mRecentPanelLeftyMode = (SwitchPreference) findPreference(RECENT_PANEL_LEFTY_MODE);
         mRecentPanelLeftyMode.setOnPreferenceChangeListener(this);
-        mIconPack = findPreference(RECENT_ICON_PACK);
     }
 
     @Override
@@ -96,6 +65,10 @@ public class SlimRecents extends SettingsPreferenceFragment
         boolean recentLeftyMode = Settings.System.getInt(getContext().getContentResolver(),
                 Settings.System.RECENT_PANEL_GRAVITY, Gravity.END) == Gravity.START;
         mRecentPanelLeftyMode.setChecked(recentLeftyMode);
+
+        IconPackPreference iconPackPref = (IconPackPreference) findPreference(RECENT_ICON_PACK);
+        // Re-initialise preference
+        iconPackPref.init();
     }
 
     @Override
@@ -111,174 +84,7 @@ public class SlimRecents extends SettingsPreferenceFragment
     }
 
     @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mIconPack) {
-            pickIconPack(getContext());
-            return true;
-        } else {
-            return super.onPreferenceTreeClick(preference);
-        }
-    }
-
-    @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.PIXELDUST;
-    }
-
-     /** Slim Recents Icon Pack Dialog **/
-    private void pickIconPack(final Context context) {
-        if (mDialog != null) {
-            return;
-        }
-        Map<String, IconPackInfo> supportedPackages = getSupportedPackages(context);
-        if (supportedPackages.isEmpty()) {
-            Toast.makeText(context, R.string.no_iconpacks_summary, Toast.LENGTH_SHORT).show();
-            return;
-        }
-        AlertDialog.Builder builder = new AlertDialog.Builder(context)
-        .setTitle(R.string.dialog_pick_iconpack_title)
-        .setOnDismissListener(this)
-        .setNegativeButton(R.string.cancel, null)
-        .setView(createDialogView(context, supportedPackages));
-        mDialog = builder.show();
-    }
-
-    private View createDialogView(final Context context, Map<String, IconPackInfo> supportedPackages) {
-        final LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final View view = inflater.inflate(R.layout.dialog_iconpack, null);
-        final IconAdapter adapter = new IconAdapter(context, supportedPackages);
-
-        mListView = (ListView) view.findViewById(R.id.iconpack_list);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                        int position, long id) {
-                if (adapter.isCurrentIconPack(position)) {
-                    return;
-                }
-                String selectedPackage = adapter.getItem(position);
-                Settings.System.putString(getContext().getContentResolver(),
-                        Settings.System.SLIM_RECENTS_ICON_PACK, selectedPackage);
-                mDialog.dismiss();
-            }
-        });
-
-        return view;
-    }
-
-    @Override
-    public void onDismiss(DialogInterface dialog) {
-        if (mDialog != null) {
-            mDialog = null;
-        }
-    }
-
-    private static class IconAdapter extends BaseAdapter {
-        ArrayList<IconPackInfo> mSupportedPackages;
-        LayoutInflater mLayoutInflater;
-        String mCurrentIconPack;
-        int mCurrentIconPackPosition = -1;
-
-        IconAdapter(Context ctx, Map<String, IconPackInfo> supportedPackages) {
-            mLayoutInflater = LayoutInflater.from(ctx);
-            mSupportedPackages = new ArrayList<IconPackInfo>(supportedPackages.values());
-            Collections.sort(mSupportedPackages, new Comparator<IconPackInfo>() {
-                @Override
-                public int compare(IconPackInfo lhs, IconPackInfo rhs) {
-                    return lhs.label.toString().compareToIgnoreCase(rhs.label.toString());
-                }
-            });
-
-            Resources res = ctx.getResources();
-            String defaultLabel = res.getString(R.string.default_iconpack_title);
-            Drawable icon = res.getDrawable(android.R.drawable.sym_def_app_icon);
-            mSupportedPackages.add(0, new IconPackInfo(defaultLabel, icon, ""));
-            mCurrentIconPack = Settings.System.getString(ctx.getContentResolver(),
-                Settings.System.SLIM_RECENTS_ICON_PACK);
-        }
-
-        @Override
-        public int getCount() {
-            return mSupportedPackages.size();
-        }
-
-        @Override
-        public String getItem(int position) {
-            return (String) mSupportedPackages.get(position).packageName;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        public boolean isCurrentIconPack(int position) {
-            return mCurrentIconPackPosition == position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = mLayoutInflater.inflate(R.layout.iconpack_view_radio, null);
-            }
-            IconPackInfo info = mSupportedPackages.get(position);
-            TextView txtView = (TextView) convertView.findViewById(R.id.title);
-            txtView.setText(info.label);
-            ImageView imgView = (ImageView) convertView.findViewById(R.id.icon);
-            imgView.setImageDrawable(info.icon);
-            RadioButton radioButton = (RadioButton) convertView.findViewById(R.id.radio);
-            boolean isCurrentIconPack = info.packageName.equals(mCurrentIconPack);
-            radioButton.setChecked(isCurrentIconPack);
-            if (isCurrentIconPack) {
-                mCurrentIconPackPosition = position;
-            }
-            return convertView;
-        }
-    }
-
-    private Map<String, IconPackInfo> getSupportedPackages(Context context) {
-        Intent i = new Intent();
-        Map<String, IconPackInfo> packages = new HashMap<String, IconPackInfo>();
-        PackageManager packageManager = context.getPackageManager();
-        for (String action : sSupportedActions) {
-            i.setAction(action);
-            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
-                IconPackInfo info = new IconPackInfo(r, packageManager);
-                packages.put(r.activityInfo.packageName, info);
-            }
-        }
-        i = new Intent(Intent.ACTION_MAIN);
-        for (String category : sSupportedCategories) {
-            i.addCategory(category);
-            for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
-                IconPackInfo info = new IconPackInfo(r, packageManager);
-                packages.put(r.activityInfo.packageName, info);
-            }
-            i.removeCategory(category);
-        }
-        return packages;
-    }
-
-    private static class IconPackInfo {
-        String packageName;
-        CharSequence label;
-        Drawable icon;
-
-        IconPackInfo(ResolveInfo r, PackageManager packageManager) {
-            packageName = r.activityInfo.packageName;
-            icon = r.loadIcon(packageManager);
-            label = r.loadLabel(packageManager);
-        }
-
-        IconPackInfo(){
-        }
-
-        public IconPackInfo(String label, Drawable icon, String packageName) {
-            this.label = label;
-            this.icon = icon;
-            this.packageName = packageName;
-        }
     }
 }
