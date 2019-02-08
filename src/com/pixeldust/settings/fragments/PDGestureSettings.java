c5b269e10f8f1be6f53d0ec0ff5c81cb822b0882
Alex <du.alexcruz@gmail.com>
2019-02-07 21:59:48 -0600

    Active edge settings 3/? Squashed in: - Add a footer to Active Edge settings
    Change-Id: I3f0a5883d171ae8c5345e7ad531f65b7d0fd0210
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/PDGestureSettings.java
+++ src/com/pixeldust/settings/fragments/PDGestureSettings.java
@@ -1,19 +1,33 @@
 package com.pixeldust.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
 import android.os.Bundle;
-import com.android.settings.R;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
 
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class PDGestureSettings extends SettingsPreferenceFragment {
+
+    private static final String ACTIVE_EDGE_CATEGORY = "active_edge_category";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.pixeldust_settings_gestures);
+
+        Preference ActiveEdge = findPreference(ACTIVE_EDGE_CATEGORY);
+        if (!getResources().getBoolean(R.bool.has_active_edge)) {
+            getPreferenceScreen().removePreference(ActiveEdge);
+        } else {
+            if (!getContext().getPackageManager().hasSystemFeature(
+                    "android.hardware.sensor.assist")) {
+                getPreferenceScreen().removePreference(ActiveEdge);
+            }
+        }
     }
 
     @Override
